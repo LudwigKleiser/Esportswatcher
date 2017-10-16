ddb425c3b98856274da5961c707b3d110d6b8b38
Ludwig Kleiser <ludde_orby@hotmail.com>
2017-10-16 18:13:26 +0200

    For sharing

--- Esportswatcher/Esportswatcher/Startup.cs
+++ Esportswatcher/Esportswatcher/Startup.cs
@@ -16,6 +16,7 @@ namespace Esportswatcher
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,13 +26,12 @@ namespace Esportswatcher
 
             if (env.IsDevelopment())
             {
+                app.UseStaticFiles();
+                app.UseMvcWithDefaultRoute();
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+           
         }
     }
 }
